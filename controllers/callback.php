7aac0c7aebd28934869f47a27b82732c52bdcf34
RainielSantos <rainielsantos7@gmail.com>
2025-05-06 15:30:24 +0800

    added chages to usm database connection and insertion on callback google api

--- controllers/callback.php
+++ controllers/callback.php
@@ -10,58 +10,61 @@ $client->setClientSecret($config['google']['client_secret']);
 $client->setRedirectUri($config['google']['redirect_uri']);
 
 if (isset($_GET['code'])) {
-    $db = new Database($config['database']);
+    $db = new Database($config['usm']);
     $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
     $client->setAccessToken($token);
 
     $oauth = new Google\Service\Oauth2($client);
     $userInfo = $oauth->userinfo->get();
-
-    $user = $db->query('SELECT * FROM user_accounts WHERE email = :email', [
+    // dd($userInfo);
+    $user = $db->query('SELECT * FROM user_account WHERE email = :email', [
         ':email' => $userInfo->email,
     ])->fetch();
 
     if (!$user) {
-        $db->query("INSERT INTO user_accounts (username, email, role, register_type) VALUES (:username, :email, :role, :register_type)", [
+        $db->query("INSERT INTO user_account (department_id, first_name, last_name, username, email, role, register_type) VALUES (:department_id, :first_name, :last_name, :username, :email, :role, :register_type)", [
+            "department_id" => 1,
+            ":first_name" => $userInfo->given_name,
+            ":last_name" => $userInfo->family_name,
             ":username" => $userInfo->name,
             ":email" => $userInfo->email,
-            ":role" => 6,
+            ":role" => 'applicant',
             ":register_type" => 'google',
         ]);
     }
 
-    $user_role = $db->query('SELECT * FROM user_accounts WHERE email = :email', [
+    $user_role = $db->query('SELECT * FROM user_account WHERE email = :email', [
         ':email' => $userInfo->email,
     ])->fetch();
 
-    if ($user_role['role'] === 2) {
+    if ($user_role['role'] === 'admin') {
         $_SESSION['user_id'] = $user_role['user_id'];
         $_SESSION['username'] = $user_role['username'];
-        $_SESSION['role'] = 'ADMIN';
+        $_SESSION['role'] = $user_role['role'];
         header("Location: /admin/");
         exit();
-    } elseif ($user_role['role'] === 3) {
+    } elseif ($user_role['role'] === 'manager') {
         $_SESSION['user_id'] = $user_role['user_id'];
         $_SESSION['username'] = $user_role['username'];
-        $_SESSION['role'] = 'Manager';
+        $_SESSION['role'] = $user_role['role'];
         header("Location: /manager/");
         exit();
-    } elseif ($user_role['role'] === 4) {
+    } elseif ($user_role['role'] === 'recruiter') {
         $_SESSION['user_id'] = $user_role['user_id'];
         $_SESSION['username'] = $user_role['username'];
-        $_SESSION['role'] = 'HR';
+        $_SESSION['role'] = $user_role['role'];
         header("Location: /hr/");
         exit();
-    } elseif ($user_role['role'] === 5) {
+    } elseif ($user_role['role'] === 'hiring manager') {
         $_SESSION['user_id'] = $user_role['user_id'];
         $_SESSION['username'] = $user_role['username'];
-        $_SESSION['role'] = 'HIRING MANAGER';
+        $_SESSION['role'] = $user_role['role'];
         header('Location: /hr_hiring/');
         exit();
-    } elseif ($user_role['role'] === 6) {
+    } elseif ($user_role['role'] === 'applicant') {
         $_SESSION['user_id'] = $user_role['user_id'];
         $_SESSION['username'] = $user_role['username'];
-        $_SESSION['role'] = 'USER';
+        $_SESSION['role'] = $user_role['role'];
         header('Location: /home');
         exit();
     }
